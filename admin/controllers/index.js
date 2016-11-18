03f9babf5af0f9169ee3ea87983bef6ef87c8ea6
WebberWu <bird23074035@gmail.com>
2016-11-18 12:41:08 +0800

    加上 dashboard

--- admin/controllers/index.js
+++ admin/controllers/index.js
@@ -1,11 +1,13 @@
 import home from './home';
 import login from './login';
+import dashboard from './dashboard';
 
 
 module.exports = function(app) {
 
     app.use('/', home);
     app.use('/', login);
+    app.use('/', dashboard);
 
     return function(req, res, next) {
         return next();
