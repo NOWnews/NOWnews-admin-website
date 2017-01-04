7479a0f612b83b77c0a10bd31e5346c2931eb65c
WebberWu <bird23074035@gmail.com>
2017-01-04 17:40:30 +0800

    頁面取得 menu 資訊

--- admin/middlewares/setLocals.js
+++ admin/middlewares/setLocals.js
@@ -2,11 +2,24 @@
  * 設定變數到 nunjucks
  */
 
+import adminMenu from '../../adminMenu.json';
+
 module.exports = () => {
 
     return (req, res, next) => {
-
         res.locals.NODE_ENV = process.env.NODE_ENV;
+
+        // 設定左選單的
+        let pathName = req._parsedUrl.pathname.split('/')[1];
+        res.locals.adminMenu = adminMenu;
+        if (adminMenu[pathName]) {
+            _.map(adminMenu, (value) => { 
+                value.status = false; return value; 
+            });
+            adminMenu[pathName].status = true;
+        }
+        console.log(res.locals.adminMenu.member.children)
+
         return next();
     };
 };
