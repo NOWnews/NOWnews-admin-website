589a16f25808051e135a410ff20410357315f57d
WebberWu <bird23074035@gmail.com>
2017-01-05 12:11:51 +0800

    pr修正

--- admin/middlewares/setLocals.js
+++ admin/middlewares/setLocals.js
@@ -10,16 +10,9 @@ module.exports = () => {
         res.locals.NODE_ENV = process.env.NODE_ENV;
 
         // 設定左選單的
-        let pathName = req._parsedUrl.pathname.split('/')[1];
+        let pathname = req._parsedUrl.pathname.split('/')[1];
         res.locals.adminMenu = adminMenu;
-        res.locals.pathName = pathName;
-        if (adminMenu[pathName]) {
-            _.map(adminMenu, (value) => { 
-                value.status = false; return value; 
-            });
-            adminMenu[pathName].status = true;
-        }
-        console.log(res.locals.adminMenu.member.children)
+        res.locals.pathname = pathname;
 
         return next();
     };
