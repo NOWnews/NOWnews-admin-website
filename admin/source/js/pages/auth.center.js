77a9a48fbeaff498d195c7e93fb75a559f9d6be2
esbb48 <esbb48@gmail.com>
2017-01-09 12:29:09 +0800

    Move js into source/js

---
+++ admin/source/js/pages/auth.center.js
@@ -0,0 +1,34 @@
+$(function () {
+    var table = $('#center-table').DataTable({
+        "columnDefs": [
+            { "visible": false, "targets": 0 }
+        ],
+        "order": [[ 0, 'asc' ]],
+        "displayLength": 50,
+        "drawCallback": function (settings) {
+            var api = this.api();
+            var rows = api.rows( {page:'current'}).nodes();
+            var last = null;
+
+            api.column(0, { page: 'current' })
+            .data()
+            .each( function(group, i) {
+                if (last !== group) {
+                    $(rows).eq(i).before(
+                        '<tr class="group"><td colspan="2">'+group+'</td></tr>'
+                    );
+                    last = group;
+                }
+            });
+        }
+    });
+
+    $('#center-table tbody').on('click', 'tr.group', function () {
+        var currentOrder = table.order()[0];
+        if ( currentOrder[0] === 0 && currentOrder[1] === 'asc') {
+            table.order([ 0, 'desc']).draw();
+        } else {
+            table.order([ 0, 'asc']).draw();
+        }
+    });
+});
