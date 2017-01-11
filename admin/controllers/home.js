837acc13b3bafa6ff90b48dc832cc4b5aa977910
esbb48 <esbb48@gmail.com>
2017-01-11 09:54:30 +0800

    change to use axios

--- admin/controllers/home.js
+++ admin/controllers/home.js
@@ -1,6 +1,5 @@
 import co from 'co';
 import express from 'express';
-// import api from '../util/api';
 let router = express.Router();
 
 const debug = require('debug')('NOWmobile:controllers:home');
@@ -10,29 +9,6 @@ const isLogin = require('../middlewares/isLogin');
 router.route('/')
     .get(isLogin, (req, res, next) => {
         return res.render('home/index');
-        // co(function*() {
-            //
-            // let result = yield [
-            //     api('category/news'),
-            //     api('news/headline'),
-            // ];
-
-            // let mainCategory = result[0];
-            // let { newsList, ads } = result[1];
-            //
-            // if(req.query.data === 'PLAYJJ'){
-            //     return res.json({ newsList });
-            // }
-            //
-            // return res.render('home/home', {
-            //     nativeAds: ads || [],
-            //     newsList,
-            //     mainCategory,
-            //     specialType: 'headline',
-            // });
-
-        // }).catch(next);
-
     });
 
 module.exports = router;
--- global.js
+++ global.js
@@ -1,6 +1,16 @@
+var axiosLib = require('axios');
+var config = require('config');
 
 global._ = require('lodash');
+
 global.NODE_ENV = process.env.NODE_ENV || 'staging';
 
 global.rootPath = __dirname;
 global.imageStorage = __dirname + '/imageStorage';
+
+
+global.axios = axiosLib.create({
+    baseURL: config.get('apiServer'),
+    timeout: 1000,
+    headers: config.get('headers'),
+});
