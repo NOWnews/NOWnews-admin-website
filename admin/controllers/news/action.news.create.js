9542b305b450deea7b922ed825d5e31179082587
WebberWu <bird23074035@gmail.com>
2017-02-03 18:29:22 +0800

    建立新聞的 js

--- admin/controllers/news/action.news.create.js
+++ admin/controllers/news/action.news.create.js
@@ -1,16 +1,30 @@
+
 import Debug from 'debug';
 const debug = Debug('NOWnews-admin-website: controllers:news:action.news.create');
+
+import htmlToText from '../../util/htmlToText';
 
 module.exports = async (req, res, next) => {
 
     try {
-        let result = await new Promise((resolve, reject) => {
-            return resolve('controllers/news/action.create.js');
-        });
+        let userId = req.session.adminUser._id;
+        let data = req.body;
 
-        debug('createdNews = %j', result);
+        data.CreatedBy = userId;
+        data.UpdatedBy = userId;
+        // 摘要大概是 120 - 150 字
+        data.summary = htmlToText(data.content).slice(0,135);
 
-        return res.json(result);
+        // 暫時還沒有圖片功能
+        delete data.MainPhoto;
+
+        let { data: news } = await axios.post('/news', data);
+
+        debug('createdNews = %j', news);
+
+        return res.redirect(`/news/${userId}`);
+
+        // return res.json(news);
     }
     catch(err) {
         return next(err);
