d0640c8dcec0a3eaa7c8132f9e93975a39a742c8
esbb48 <esbb48@gmail.com>
2017-02-06 17:44:32 +0800

    sample for diff

--- admin/controllers/news/page.news.compare.js
+++ admin/controllers/news/page.news.compare.js
@@ -1,4 +1,5 @@
 import Debug from 'debug';
+import htmldiff from 'htmldiff/src/htmldiff.js';
 const debug = Debug('NOWnews-admin-website: controllers:news:page.news.compare');
 
 module.exports = async (req, res, next) => {
@@ -7,9 +8,30 @@ module.exports = async (req, res, next) => {
 
         let { beforeLogId, afterLogId } = req.query;
 
-        let url = `/newslog/compare?beforeLogId=${beforeLogId}&afterLogId=${afterLogId}`;
+        // let url = `/newslog/compare?beforeLogId=${beforeLogId}&afterLogId=${afterLogId}`;
 
-        let { data: result } = await axios.get(url);
+        // let { data: { before, after} } = await axios.get(url);
+
+        // temp code
+        let before = {
+            _id: 1,
+            title: 123,
+            content: '<p>this is some text 要消失的</p><br/><ul><li>1</li><li>增加</li></span>',
+        };
+
+        let after = {
+            _id: 2,
+            title: 456,
+            content: '<p>this is some 要增加的 text</p><br/><ul><li>增加</li></span>',
+        };
+
+        let content = htmldiff(before.content, after.content);
+
+        let result = {
+            before,
+            after,
+            content,
+        }
 
         debug('result = %j', result);
 
