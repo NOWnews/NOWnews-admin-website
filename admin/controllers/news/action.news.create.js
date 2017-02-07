5883997bdcd6ca60ccd87d631a84c71aca9a535c
WebberWu <bird23074035@gmail.com>
2017-02-07 15:30:06 +0800

    調整 create js

--- admin/controllers/news/action.news.create.js
+++ admin/controllers/news/action.news.create.js
@@ -22,9 +22,7 @@ module.exports = async (req, res, next) => {
 
         debug('createdNews = %j', news);
 
-        return res.redirect(`/news/${userId}`);
-
-        // return res.json(news);
+        return res.redirect(`/news/${data.id}`);
     }
     catch(err) {
         return next(err);
