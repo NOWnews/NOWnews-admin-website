08b580f4d00f3dcfc38d66eee3c5413c0f407662
WebberWu <bird23074035@gmail.com>
2017-02-07 15:30:27 +0800

    調整 update

--- admin/controllers/news/action.news.update.js
+++ admin/controllers/news/action.news.update.js
@@ -4,13 +4,18 @@ const debug = Debug('NOWnews-admin-website: controllers:news:action.news.update'
 module.exports = async (req, res, next) => {
 
     try {
-        let result = await new Promise((resolve, reject) => {
-            return resolve('controllers/news/action.update.js');
-        });
+        let { newsId } = req.params;
+        let userId = req.session.adminUser._id;
+        let data = req.body;
 
-        debug('updatedNews = %j', result);
+        data.UpdatedBy = userId;
 
-        return res.json(result);
+        let newsStatus = data.status.toLowerCase();
+        let { data: news } = await axios.put(`/news/${newsId}/${newsStatus}`, data);
+
+        debug('updatedNews = %j', news);
+
+        return res.redirect(`/news/${newsId}`);
     }
     catch(err) {
         return next(err);
