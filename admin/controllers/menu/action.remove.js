166802aaebbf2d2d5c9432903c1901cd0ac91bbd
WebberWu <bird23074035@gmail.com>
2017-02-09 00:09:56 +0800

    加上 remove 的 route

--- admin/controllers/menu/action.remove.js
+++ admin/controllers/menu/action.remove.js
@@ -4,11 +4,14 @@ const debug = Debug('NOWnews-admin-website: controllers:menu:action.remove');
 module.exports = async (req, res, next) => {
 
     try {
+        let { menuId } = req.params;
 
-        // let { data: { users: userList, pageData } } = await axios.get('/users');
+        let { data: menu } = await axios.delete( `/menu/${menuId}` );
 
-        // // return res.json(userList);
-        return res.render('menu/page.menu.create.html');
+        debug('removedMenu = %j', menu);
+
+        return res.json({ menu });
+
     }
     catch(err) {
         return next(err);
