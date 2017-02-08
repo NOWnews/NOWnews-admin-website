3da3cc267e02404f77a6d2617e38cd2e4424e0bd
wayne1025 <wayne1025.tw@hotmail.com>
2017-02-08 17:33:40 +0800

    增加menu create頁面

--- admin/controllers/menu/index.js
+++ admin/controllers/menu/index.js
@@ -3,8 +3,12 @@ import express from 'express';
 let router = express.Router();
 
 import pageList from './page.list';
+import pageMenuCreate from './page.menu.create';
 
 router.route('/')
     .get(pageList);
+
+router.route('/create')
+    .get(pageMenuCreate);
 
 module.exports = router;
