c988fa090fe26bc98fccc26a1da2b2b1c0dfc0de
wayne1025 <wayne1025.tw@hotmail.com>
2017-02-13 19:22:00 +0800

    增加menu排序更新

--- admin/controllers/menu/action.sort.update.js
+++ admin/controllers/menu/action.sort.update.js
@@ -4,11 +4,15 @@ const debug = Debug('NOWnews-admin-website: controllers:menu:action.sort.update'
 module.exports = async (req, res, next) => {
 
     try {
+        let userId = req.session.adminUser._id;
+        let data = req.body;
 
-        // let { data: { users: userList, pageData } } = await axios.get('/users');
+        debug('updatedSort = %j', data);
+        let { data: result } = await axios.put('menus/sort', { menus: data});
 
-        // // return res.json(userList);
-        return res.render('menu/page.menu.create.html');
+        return res.json({
+            result
+        });
     }
     catch(err) {
         return next(err);
