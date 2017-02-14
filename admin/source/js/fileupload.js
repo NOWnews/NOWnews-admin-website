b8c733b855fb46c51debc9f48905e359837ab059
esbb48 <esbb48@gmail.com>
2017-02-15 01:56:22 +0800

    rename and refactor and fixbug

--- admin/source/js/fileupload.js
+++ admin/source/js/fileupload.js
@@ -2,37 +2,45 @@
 
 $(function () {
     'use strict';
-    var URL = window.URL || window.webkitURL;
+
+    var apiServ = 'http://61.67.121.56:10000/';
     var fileuploadElm = $('#fileupload');
-    // 預設查詢日期
-    var today = moment().format('YYYY-MM-DD');
-    var prev2Month = moment().subtract(2, 'months').format('YYYY-MM-DD');
+    var fileRows = $('tbody.files');
+    var imageBlocks = $('.image-blocks');
     var imageLibQueryForm = $('#image-library-query-form');
-    imageLibQueryForm.find('input[name=startedAt]').val(prev2Month);
-    imageLibQueryForm.find('input[name=startedAt]').attr('max', today);
-    imageLibQueryForm.find('input[name=endedAt]').val(today);
-    imageLibQueryForm.find('input[name=endedAt]').attr('max', today);
+    var URL = window.URL || window.webkitURL;
+
+    initQueryFrom();
+
+    function initQueryFrom() {
+        var today = moment().format('YYYY-MM-DD');
+        var prev2Month = moment().subtract(2, 'months').format('YYYY-MM-DD');
+        var startElm = imageLibQueryForm.find('input[name=startedAt]');
+        var endElm = imageLibQueryForm.find('input[name=endedAt]');
+        startElm.val(prev2Month);
+        startElm.attr('max', today);
+        endElm.val(today);
+        endElm.attr('max', today);
+    }
 
 
     // Initialize the jQuery File Upload widget:
     fileuploadElm.fileupload({
-        url: 'http://61.67.121.56:10000/images/upload',
-        paramName: 'image',
-        uploadTemplateId: null,
         downloadTemplateId: null,
-        uploadTemplate: function (o) {
-            var rows = $();
-            $.each(o.files, function (index, file) {
-                var blobURL = URL.createObjectURL(file);
-                var row = $($('#template-upload').html());
-                row.find('.name').text(file.name);
-                row.find('button.crop').attr('data-blobURL', blobURL);
-                if (file.error) {
-                    row.find('.error').text(file.error);
+        paramName: 'image',
+        url: apiServ + 'images/upload',
+        uploadTemplateId: null,
+        destroy: function (e, data) {
+            var that = this;
+            // 自己上傳的當下可以真刪除
+            $.ajax({
+                url: '/image/' + data.id + '/realRemove',
+                type: 'DELETE',
+                success: function(result) {
+                    // 再去呼叫原生 library destroy 該做的事情
+                    $.blueimp.fileupload.prototype.options.destroy.call(that, e, data);
                 }
-                rows = rows.add(row);
             });
-            return rows;
         },
         downloadTemplate: function (o) {
             var rows = $();
@@ -43,38 +51,41 @@ $(function () {
                 row.find('img').attr('src', file.url);
 
                 if (file.error) {
-                    row.find('.delete').addClass("hidden");
-                    row.find('.cancel').removeClass("hidden");
+                    row.find('.delete').addClass('hidden');
+                    row.find('.cancel').removeClass('hidden');
                     row.find('.error').text(file.error);
                 }
                 rows = rows.add(row);
             });
             return rows;
         },
-        destroy: function (e, data) {
-            var that = this;
-            // 自己上傳的當下可以真刪除
-            $.ajax({
-                url: '/image/' + data.id + '/realRemove',
-                type: 'DELETE',
-                success: function(result) {
-                    $.blueimp.fileupload.prototype.options.destroy.call(that, e, data);
-                }
-            });
+        getFilesFromResponse: function (data) {
+            return [data.result];
         },
         submit: function (e, data) {
-            var file = data.files[0];
-            var elmName = $('p.name:contains("' + file.name + '")');
-            var desc = elmName.parent().find('input[name=desc]').val();
+            var desc = $(data.context).find('input[name=desc]').val();
             data.formData = {
                 desc: desc,
                 type: 'NEWS',
                 CreatedBy: $('#userId').val(),
             };
         },
-        getFilesFromResponse: function (data) {
-            return [data.result];
+        uploadTemplate: function (o) {
+            var rows = $();
+            $.each(o.files, function (index, file) {
+                var blobURL = URL.createObjectURL(file);
+                var row = $($('#template-upload').html());
+                row.find('button.crop').attr('data-blobURL', blobURL);
+                if (file.error) {
+                    row.find('.crop').addClass('hidden');
+                    row.find('.start').addClass('hidden');
+                    row.find('.error').text(file.error);
+                }
+                rows = rows.add(row);
+            });
+            return rows;
         },
+
     });
 
     fileuploadElm.bind('fileuploadcompleted', function (e, data) {
@@ -82,9 +93,8 @@ $(function () {
         $('.zoom').zoom();
     });
 
-
-
-    $('tbody.files').on('click', 'button.crop', function(){
+    // 上傳前的圖片切割
+    fileRows.on('click', 'button.crop', function(){
         var cropButton = $(this);
         var cropRow = $($('#template-crop').html());
         var cropImgElm = cropRow.find('img');
@@ -100,7 +110,7 @@ $(function () {
 
     });
 
-    $('tbody.files').on('click', 'button.confirm', function(){
+    fileRows.on('click', 'button.confirm', function(){
         var confirmButton = $(this);
         var imageRow = confirmButton.closest('#crop-row').prev();
         var cropImgElm = confirmButton.parent().parent().find('#crop-img');
@@ -114,8 +124,10 @@ $(function () {
         });
     });
 
+
+    // Image Library
     imageLibQueryForm.submit(function(e) {
-        $('.image-blocks').html("");
+        imageBlocks.html('');
 
         $.get('/image?' + $(this).serialize(), function(result) {
             $('.zoom').trigger('zoom.destroy');
@@ -124,7 +136,7 @@ $(function () {
                 block.find('img').attr('src', image.url);
                 block.find('.desc').text(image.desc);
                 block.find('.fa-trash').attr('data-id', image._id);
-                $('.image-blocks').append(block);
+                imageBlocks.append(block);
             });
             $('.zoom').zoom();
 
@@ -132,7 +144,7 @@ $(function () {
         return false;
     });
 
-    $('.image-blocks').on('click', '.fa-trash', function(e, c) {
+    imageBlocks.on('click', '.fa-trash', function(e, c) {
         // 從 library 取得是假刪除
         var confirmed = confirm('您確定要刪除嗎？');
         if (!confirmed) {
