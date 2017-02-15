78fc5b81c8f2fc5de4ed5ae53b257d9ebc427333
esbb48 <esbb48@gmail.com>
2017-02-15 12:05:38 +0800

    prevent Avatar is undefined

--- admin/controllers/auth/action.login.js
+++ admin/controllers/auth/action.login.js
@@ -29,7 +29,7 @@ module.exports = async (req, res, next) => {
             ...adminUser
         } = loginUser;
 
-        adminUser.avatarUrl = Avatar.url;
+        adminUser.avatarUrl = Avatar ? Avatar.url : '';
 
         adminUser.Center = {
             _id: Center._id,
