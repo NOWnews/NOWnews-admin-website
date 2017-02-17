1977da56943aa78afab594e6c9b99aaf4c0bf88a
esbb48 <esbb48@gmail.com>
2017-02-17 23:05:43 +0800

    update ui and remove no need code

--- admin/controllers/news/action.news.create.js
+++ admin/controllers/news/action.news.create.js
@@ -19,9 +19,6 @@ module.exports = async (req, res, next) => {
         // 摘要大概是 120 - 150 字
         data.summary = htmlToText(data.content).slice(0,135);
 
-        // 暫時還沒有圖片功能
-        delete data.MainPhoto;
-
         // Tags 的處理
         let tags;
         if (data.tags !== ''){
