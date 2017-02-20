833c449c2460d5d1ed56af04549e717b1c3259cb
Simon Sun <blackghost0118@gmail.com>
2017-02-20 09:15:32 +0800

    中心與部門的判斷要更加詳細

--- admin/controllers/auth/action.login.js
+++ admin/controllers/auth/action.login.js
@@ -31,15 +31,27 @@ module.exports = async (req, res, next) => {
 
         adminUser.avatarUrl = Avatar ? Avatar.url : null ;
 
-        adminUser.Center = {
-            _id: Center._id,
-            name: Center.name,
-        };
+        // adminUser.Center = {
+        //     _id: Center._id,
+        //     name: Center.name,
+        // };
+        if(Center) {
+            adminUser.Center = {
+                _id: Center._id,
+                name: Center.name,
+            };
+        }
 
-        adminUser.Department = {
-            _id: Department._id,
-            name: Department.name,
-        };
+        // adminUser.Department = {
+        //     _id: Department._id,
+        //     name: Department.name,
+        // };
+        if(Department) {
+            adminUser.Department = {
+                _id: Department._id,
+                name: Department.name,
+            };
+        }
 
         adminUser.Role = {
             _id: Role._id,
