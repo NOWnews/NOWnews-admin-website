6c27f1459115eb3624f9ae90a8d12c2c61221453
esbb48 <esbb48@gmail.com>
2017-02-20 16:35:37 +0800

    use let

--- admin/middlewares/isLogin.js
+++ admin/middlewares/isLogin.js
@@ -3,7 +3,7 @@ import co from 'co';
 const debug = require('debug')('NOWadmin:admin:middlewares:isLogin');
 
 module.exports = function(req, res, next) {
-    var isLogin = (req.session && req.session.adminUser);
+    let isLogin = (req.session && req.session.adminUser);
 
     if ((req.path !== '/auth/login' && req.path !== '/auth/logout' ) && !isLogin) {
         return res.redirect('/auth/login');
