f8c69096a50412cc031de8c190124c10e7142788
WebberWu <bird23074035@gmail.com>
2017-02-20 11:20:30 +0800

    變更審核中新聞

--- admin/controllers/news/page.news.takeMeReviewList.js
+++ admin/controllers/news/page.news.takeMeReviewList.js
@@ -8,7 +8,7 @@ module.exports = async (req, res, next) => {
         let { data: newsListInfo } = await axios.get('/news', {
             params: {
                 status: 'REVIEW',
-                LastReviewer: userId
+                Reviewer: userId
             }
         });
 
