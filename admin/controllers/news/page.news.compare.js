1a488cbc00a56655ed985db44730aeb9d12464ca
esbb48 <esbb48@gmail.com>
2017-02-24 11:25:13 +0800

    Support Tag

--- admin/controllers/news/page.news.compare.js
+++ admin/controllers/news/page.news.compare.js
@@ -1,4 +1,5 @@
 import Debug from 'debug';
+import _ from 'lodash';
 import { NEWS_TYPES, NEWS_STATUS } from '../../util/constants';
 
 const debug = Debug('NOWnews-admin-website: controllers:news:page.news.compare');
@@ -12,6 +13,22 @@ module.exports = async (req, res, next) => {
         let url = `/newslog/compare?beforeLogId=${beforeLogId}&afterLogId=${afterLogId}`;
 
         let { data: { after, before } } = await axios.get(url);
+
+        // 將 ArrayObject 轉成只有名字的字串 (aa,bb,cc)
+        function generateArrayString (target, columnName) {
+
+            let values = _.map(target[columnName], function(value) {
+                return value.name;
+            });
+            target[columnName] = null;
+            target[`${columnName}String`] = values.join(',');
+        }
+
+        generateArrayString(before, 'Tags');
+        generateArrayString(after, 'Tags');
+        generateArrayString(before, 'Menus');
+        generateArrayString(after, 'Menus');
+
 
         let result = {
             after,
