c6a74308a59e7577a41fa8f5646d8f9c095c19c3
esbb48 <esbb48@gmail.com>
2017-02-24 13:52:00 +0800

    refactor

--- admin/controllers/news/page.news.compare.js
+++ admin/controllers/news/page.news.compare.js
@@ -36,7 +36,7 @@ module.exports = async (req, res, next) => {
             before,
             NEWS_TYPES,
             NEWS_STATUS,
-        }
+        };
 
         debug('result = %j', result);
 
