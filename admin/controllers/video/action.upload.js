f8bde45ff8071edbb8cb99ab5fc46c6fd673a583
WebberWu <bird23074035@gmail.com>
2017-03-07 17:11:28 +0800

    ✨ Feat: add upload video

--- admin/controllers/video/action.upload.js
+++ admin/controllers/video/action.upload.js
@@ -13,11 +13,6 @@ module.exports = async (req, res, next) => {
         let newPath = `uploads/${originalname}`;
         fs.renameSync(path, newPath);
 
-        console.log('L19', originalname)
-        console.log('L20', path)
-        console.log('L21', newPath)
-        console.log('L22', req.body)
-
         //取得 FormData 和 Headers
         let { formData, headers } = await new Promise((resolve) => {
             let { title, desc, type, isDeliver } = req.body;
@@ -27,29 +22,26 @@ module.exports = async (req, res, next) => {
             fd.append('type', type);
             fd.append('isDeliver', isDeliver);
             fd.append('CreatedBy', userId);
-            fd.append('image', fs.createReadStream(newPath));
+            fd.append('video', fs.createReadStream(newPath));
             fd.pipe(concat(
                 { encoding: 'buffer' },
                 formData => resolve({ formData, headers: fd.getHeaders() })
             ));
         });
-        return false;
+
+        let { data: video } = await axios.post('/videos/upload', formData, { headers });
+
+        debug('video = %j', video);
 
         // 刪掉檔案
-        // await new Promise((resolve, reject) => {
-        //     fs.unlink(newPath, (err, result) => {
-        //         if(err) {
-        //             return reject(err);
-        //         }
-        //         return resolve(result);
-        //     });
-        // });
-
-        // return res.json(newImage);
-
-        // let { data: { video } } = await axios.post(`/video/upload`);
-
-        // debug('video = %j', video);
+        await new Promise((resolve, reject) => {
+            fs.unlink(newPath, (err, result) => {
+                if(err) {
+                    return reject(err);
+                }
+                return resolve(result);
+            });
+        });
 
         return res.json({ video });
     }
