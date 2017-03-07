b1acdc337d6d7a609ffdec65d705a3308ca39f90
WebberWu <bird23074035@gmail.com>
2017-03-07 17:45:53 +0800

    add create video url

---
+++ admin/controllers/video/action.create.js
@@ -0,0 +1,27 @@
+import concat from 'concat-stream';
+import Debug from 'debug';
+import fs from 'fs';
+const debug = Debug('NOWnews-admin-website: controllers:video:action.create');
+
+module.exports = async (req, res, next) => {
+
+    try {
+        let { _id: userId } = req.session.adminUser;
+
+        let { url, title, desc, type, Tags, CreatedBy } = req.body;
+        let data = req.body;
+
+        if (!Tags) {
+            delete data.Tags;
+        }
+
+        let { data: video } = await axios.post('/videos', data);
+
+        debug('video = %j', video);
+
+        return res.json({ video });
+    }
+    catch(err) {
+        return next(err);
+    }
+};
