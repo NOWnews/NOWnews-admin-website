886c403a9ddbb9ef975777f2cdfafacf121cf0eb
WebberWu <bird23074035@gmail.com>
2017-03-10 09:28:13 +0800

    🐛 Fix: change value

--- admin/util/constants.js
+++ admin/util/constants.js
@@ -7,8 +7,8 @@ module.exports = {
     },
     NEWS_TYPES: {
         NEWS: '一般新聞',
-        VIDEO: '圖片新聞',
-        PHOTO: '影音新聞'
+        VIDEO: '影音新聞',
+        PHOTO: '圖片新聞'
     },
     NEWS_STATUS: {
         DRAFT: '草稿',
