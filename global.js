30fdd7227037e6abee229d89cd9d1af097297937
WebberWu <bird23074035@gmail.com>
2017-03-10 12:20:11 +0800

    🐛 Fix: change default time

--- global.js
+++ global.js
@@ -11,6 +11,6 @@ global.imageStorage = __dirname + '/imageStorage';
 
 global.axios = axiosLib.create({
     baseURL: config.get('apiServer'),
-    timeout: 8000,
+    timeout: 5000,
     headers: config.get('headers'),
 });
