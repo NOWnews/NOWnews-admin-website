9211d6a80e081bf3f42148c041ab9ee7f85caf4e
WebberWu <bird23074035@gmail.com>
2017-03-13 19:42:42 +0800

    ✨ Feat: add video edit text and title func
    close#133

--- admin/controllers/video/index.js
+++ admin/controllers/video/index.js
@@ -5,12 +5,16 @@ let router = express.Router();
 let videoUpload = multer({ dest: 'uploads/' });
 
 import actionRemove from './action.remove';
+import actionUpdate from './action.update';
 import actionUpload from './action.upload';
 import actionCreate from './action.create';
 import actionFind from './action.find';
 
 router.route('/remove/:id')
     .delete(actionRemove);
+
+router.route('/update/:id')
+    .put(actionUpdate);
 
 router.route('/')
     .post(actionCreate)
