50b6a9d8d57fbd9bc39be2cf34dedb1e003cc9c0
wayne1025 <wayne1025.tw@hotmail.com>
2017-03-20 20:40:16 +0800

    ✨ Feat: add news for move table use pre-news pre-topic pre-specialchannel pre-video

--- admin/controllers/indexpage/page.indexpage.create.js
+++ admin/controllers/indexpage/page.indexpage.create.js
@@ -9,8 +9,20 @@ module.exports = async (req, res, next) => {
         let baseURL = config.get('apiServer');
         let userId = req.session.adminUser._id;
         let { data: indexpage } = await axios.get(baseURL+'/indexpage');
+
+        let { data: predata } = await axios.get(baseURL+'/news?limit=20&status=RELEASE&type=NEWS');
+        let { data: pretopic } = await axios.get(baseURL+'/specialtopics?limit=20');
+        let { data: prespecialchannel } = await axios.get(baseURL+'/specialchannels?limit=20');
+        let { data: prevideo } = await axios.get(baseURL+'/news?limit=20&status=RELEASE&type=VIDEO');
+
         debug('indexpageList = %j', indexpage );
-        return res.render('indexpage/page.create.html', indexpage);
+        return res.render('indexpage/page.create.html', {
+            indexpage,
+            predata,
+            pretopic,
+            prespecialchannel,
+            prevideo
+        });
     }
     catch(err) {
         return next(err);
