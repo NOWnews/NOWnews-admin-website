9533bdde9c9dbcbd1fc60014342abb0eb3b66b0e
wayne1025 <wayne1025.tw@hotmail.com>
2017-03-20 21:18:27 +0800

    🐛 Fix: modify code refractory use smart method

--- admin/controllers/indexpage/page.indexpage.create.js
+++ admin/controllers/indexpage/page.indexpage.create.js
@@ -8,12 +8,20 @@ module.exports = async (req, res, next) => {
     try{
         let baseURL = config.get('apiServer');
         let userId = req.session.adminUser._id;
-        let { data: indexpage } = await axios.get(baseURL+'/indexpage');
 
-        let { data: predata } = await axios.get(baseURL+'/news?limit=20&status=RELEASE&type=NEWS');
-        let { data: pretopic } = await axios.get(baseURL+'/specialtopics?limit=20');
-        let { data: prespecialchannel } = await axios.get(baseURL+'/specialchannels?limit=20');
-        let { data: prevideo } = await axios.get(baseURL+'/news?limit=20&status=RELEASE&type=VIDEO');
+        let results = await Promise.all([
+            axios.get(baseURL+'/indexpage'),
+            axios.get(baseURL+'/news?limit=20&status=RELEASE&type=NEWS'),
+            axios.get(baseURL+'/specialtopics?limit=20'),
+            axios.get(baseURL+'/specialchannels?limit=20'),
+            axios.get(baseURL+'/news?limit=20&status=RELEASE&type=VIDEO')
+        ]);
+
+        let indexpage = results[0].data;
+        let predata = results[1].data;
+        let pretopic = results[2].data;
+        let prespecialchannel = results[3].data;
+        let prevideo = results[4].data;
 
         debug('indexpageList = %j', indexpage );
         return res.render('indexpage/page.create.html', {
