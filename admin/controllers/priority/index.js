48a81a2c7cff0308addfa9d7e318e569f31344db
wayne1025 <wayne1025.tw@hotmail.com>
2017-04-18 19:54:16 +0800

    ✨ Feat: add html table editpage

--- admin/controllers/priority/index.js
+++ admin/controllers/priority/index.js
@@ -3,9 +3,12 @@ import express from 'express';
 let router = express.Router();
 
 import pagePriorityList from './page.priority.list';
+import pagePriorityGetNewsByCategory from './page.priorityGetNewslistByCatgory';
 
 router.route('/')
     .get(pagePriorityList);
 
+router.route('/getnews')
+    .get(pagePriorityGetNewsByCategory);
 
 module.exports = router;
