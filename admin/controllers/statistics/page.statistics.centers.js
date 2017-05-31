c02132b8691b71904de4c221d64ee791126f9b94
wayne1025 <wayne1025.tw@hotmail.com>
2017-05-31 18:48:08 +0800

    ✨ Feat: modify url by started and ended time to url

--- admin/controllers/statistics/page.statistics.centers.js
+++ admin/controllers/statistics/page.statistics.centers.js
@@ -1,10 +1,20 @@
 import Debug from 'debug';
+import moment from 'moment-timezone';
+
 const debug = Debug('NOWnews-admin-website: controllers:statistics:page.centersList');
+
+
 
 module.exports = async (req, res, next) => {
 
     try{
         let query = req.query;
+
+        if (!query.startedAt) {
+            query.startedAt = moment().tz('Asia/Taipei').format('YYYY-MM-DD');
+            query.endedAt = moment().tz('Asia/Taipei').format('YYYY-MM-DD');
+        }
+
         let queryString = req._parsedUrl.query;
 
         let userId = req.session.adminUser._id;
