f45e4554e83e1d6016958433c6ee8dbe97ad93e0
JaosnHsieh <appleoxxo@gmail.com>
2017-06-19 18:18:42 +0800

    🐛 Fix: adjust dailyplan timezone and ui

--- admin/controllers/dailyplan/action.create.js
+++ admin/controllers/dailyplan/action.create.js
@@ -1,4 +1,5 @@
 import Debug from 'debug';
+import moment from 'moment';
 const debug = Debug('NOWnews-admin-website: controllers:dailyplan:action.create');
 
 module.exports = async (req, res, next) => {
@@ -6,6 +7,7 @@ module.exports = async (req, res, next) => {
     try {
 
         let { title, startedAt, Center, content,  } = req.body;
+        startedAt = moment(startedAt).valueOf();
         let CreatedBy = req.session.adminUser._id;
         let { data: dailyPlan } = await axios.post('/dailyPlan',
             { title, startedAt, Center, content, CreatedBy });
