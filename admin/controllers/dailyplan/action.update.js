275dbbb5223b4717e89774de84783923e8011af8
JaosnHsieh <appleoxxo@gmail.com>
2017-06-21 07:18:35 -0500

    🐛 Fix: fix wrong time format

--- admin/controllers/dailyplan/action.update.js
+++ admin/controllers/dailyplan/action.update.js
@@ -9,7 +9,7 @@ module.exports = async (req, res, next) => {
         let { id } = req.params;
 
         let { title, startedAt, Center, content } = req.body;
-        startedAt = moment.tz(startedAt,'YYYY-MM-DDTHH:mm:ss','Asia/Taipei');
+        startedAt = moment.tz(startedAt,'YYYY-MM-DDTHH:mm:ss','Asia/Taipei').format('YYYY-MM-DDTHH:mm:ss');
         let UpdatedBy = req.session.adminUser._id;
 
         let { data: dailyPlan } = await axios.put(`/dailyPlan/${id}`,
