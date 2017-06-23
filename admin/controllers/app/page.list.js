b9f82af13f20356df065636a282ea33846dcb51a
Simon Sun <blackghost0118@gmail.com>
2017-06-23 21:30:54 +0800

    :star2: add app version create and list

---
+++ admin/controllers/app/page.list.js
@@ -0,0 +1,16 @@
+import Debug from 'debug';
+const debug = Debug('NOWnews-admin-website:controllers:app:page.list');
+
+module.exports = async (req, res, next) => {
+    try{
+        let { data: appInfoList } = await axios.get(`/app/version`);
+        debug('app info list = %j', appInfoList);
+
+        return res.render('app/page.list.html', {
+            appInfoList
+        });
+    }
+    catch(err) {
+        return next(err);
+    }
+};
