16e29b9c70357dde4b62e610ad063037761e67ce
Simon Sun <blackghost0118@gmail.com>
2017-06-26 02:30:59 +0800

    :star2: add app notification page and action

--- admin/controllers/app/index.js
+++ admin/controllers/app/index.js
@@ -5,8 +5,11 @@ let imageUpload = multer({ dest: 'uploads/' });
 
 import pageversionList from './page.versionList';
 import pageSplashList from './page.splashList';
+import pageNotification from './page.notification';
 import actionVersionCreate from './action.versionCreate';
 import actionSplashCreate from './action.splashCreate';
+import actionNotification from './action.notification';
+
 
 let router = Router();
 
@@ -17,5 +20,9 @@ router.route('/version')
 router.route('/splash')
     .get(pageSplashList)
     .post(imageUpload.single('image'), actionSplashCreate);
+
+router.route('/notification')
+    .get(pageNotification)
+    .post(actionNotification);
 
 module.exports = router;
