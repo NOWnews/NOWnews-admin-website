63df7c76515c826fb93da39783d6e57c3160034e
JaosnHsieh <appleoxxo@gmail.com>
2017-06-29 22:00:05 +0800

    🐛 Fix: change topics list click url

--- admin/controllers/topic/page.topic.list.js
+++ admin/controllers/topic/page.topic.list.js
@@ -1,17 +1,18 @@
 import Debug from 'debug';
 const debug = Debug('NOWnews-admin-website: controllers:topic:page.list');
-
+import config from 'config';
 module.exports = async (req, res, next) => {
 
     try{
-
         let userId = req.session.adminUser._id;
         let { data: topic } = await axios.get('/specialtopics');
 
         let topics = topic;
+        topic.officialUrl = config.get('officialUrl');
+
         debug('topicList = %j', topics );
 
-        return res.render('topic/page.list.html', topics);
+        return res.render('topic/page.list.html',topics);
     }
     catch(err) {
         return next(err);
