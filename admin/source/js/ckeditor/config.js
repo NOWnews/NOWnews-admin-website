fe91607f965dd9592ca5d1184d255433bb27dd9e
Webber <bird23074035@gmail.com>
2017-07-05 09:56:31 +0800

    🐛 Fix: forcePasteAsPlainText = false

--- admin/source/js/ckeditor/config.js
+++ admin/source/js/ckeditor/config.js
@@ -24,7 +24,7 @@ CKEDITOR.editorConfig = function( config ) {
 	config.removeDialogTabs = 'image:advanced;link:advanced';
 
 	//一般貼上時 強迫純文字
-	config.forcePasteAsPlainText = true;
+	config.forcePasteAsPlainText = false;
 
 	//嵌入影片需要額外允許的tag
 	config.extraAllowedContent = 'script;blockquote(*);img[data-isdeliver];iframe[*]';
