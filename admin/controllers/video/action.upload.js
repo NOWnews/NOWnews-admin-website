b91c5241273386e12128db9ce14d07fe0fdf405a
JaosnHsieh <appleoxxo@gmail.com>
2017-08-23 11:19:14 +0800

    🔨 Refactor:

--- admin/controllers/video/action.upload.js
+++ admin/controllers/video/action.upload.js
@@ -8,8 +8,8 @@ module.exports = async (req, res, next) => {
     try {
         // 影片檔案若太大等太久連線會斷 延長斷線時間至30分鐘
         req.socket.setTimeout(30 * 60 * 1000);
-        req.socket.addListener('timeout', function() {
-          req.socket.destroy();
+        req.socket.addListener('timeout', () => {
+            req.socket.destroy();
         });
 
         let { _id: userId } = req.session.adminUser;
