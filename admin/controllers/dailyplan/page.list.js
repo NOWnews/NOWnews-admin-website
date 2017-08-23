ac5ef9cf9948ee21e4f3efaf4b308d783c5a92c6
JaosnHsieh <appleoxxo@gmail.com>
2017-08-23 19:11:40 +0800

    🐛 Fix: add all users list to dailyPlan list

--- admin/controllers/dailyplan/page.list.js
+++ admin/controllers/dailyplan/page.list.js
@@ -1,6 +1,8 @@
 import moment from 'moment-timezone';
 import Debug from 'debug';
 import qs from 'querystring';
+import fs from 'fs';
+import _ from  'lodash';
 const debug = Debug('NOWnews-admin-website: controllers:dailyplan:page.list');
 
 module.exports = async (req, res, next) => {
@@ -20,30 +22,36 @@ module.exports = async (req, res, next) => {
         }
 
         if(!qsObj.limit){
-            qsObj.limit = 40;
+            qsObj.limit = 1000;
         }
         let queryString = qs.stringify(qsObj);
-
-        let { data: {dailyPlans, pageData} } = await axios.get(`/dailyPlan?${queryString}`);
 
         //只取新聞部底下的中心
         let { data : departments } = await axios.get('/departments');
         departments = _.filter(departments,(department)=>{
             return department.name.indexOf('新聞部')>-1;
         });
+        let newsDepartmentId = departments[0] ? departments[0]._id : '';
 
-        let qsNoPage = qs.parse(req._parsedUrl.query);
-        delete qsNoPage.page;
-        qsNoPage = qs.stringify(qsNoPage);
-        pageData.qsNoPage = qsNoPage?'&'+qsNoPage:'';
+        let [
+            { data : { dailyPlans } },
+            { data : { users }}
+        ] = await Promise.all([
+            axios.get(`/dailyPlan?${queryString}`),
+            axios.get(`/users?Department=${newsDepartmentId}&Center=${qsObj.Center}&limit=1000`)
+        ]);
+
+        dailyPlans = _.groupBy(dailyPlans,"CreatedBy._id");
+        users = _.forEach(users, ( user )=>{
+            user.dailyPlans = dailyPlans[user._id] || [];
+        });
 
         debug('dailyPlan = %j', dailyPlans);
 
         return res.render('dailyPlan/page.list.html', {
+            users,
             qsObj,
-            departments,
-            dailyPlans,
-            pageData
+            departments
         });
     }
     catch(err) {
