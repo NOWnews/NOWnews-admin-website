3932e95526aa67f4b4ec0e61cc4ae83491bb8d22
esbb48 <esbb48@gmail.com>
2017-09-11 16:05:20 +0800

    ✨ Feat: Fix bug

--- admin/controllers/column/action.specialchannel.update.js
+++ admin/controllers/column/action.specialchannel.update.js
@@ -9,11 +9,20 @@ module.exports = async (req, res, next) => {
         let userId = req.session.adminUser._id;
         let { Menu, SubMenus } = req.body;
 
+        // Prevent it's string
+        if (typeof SubMenus === 'string') {
+            SubMenus = [SubMenus];
+        }
+
         const repeatMenuIndex = SubMenus.indexOf(Menu);
 
         //  避免主分類重複拉到子項
         if (repeatMenuIndex !== -1) {
             SubMenus.splice(repeatMenuIndex, 1);
+        }
+
+        if (SubMenus.length === 0) {
+            throw({ data: '子項不可以是空的 / 講同一個分類同時設為主與子項' });
         }
 
         const data = {
