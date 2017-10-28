b52b8eb591f3f2e77ac75707610985874e4c9519
esbb48 <esbb48@gmail.com>
2017-10-29 01:21:32 +0800

    ✨ Feat: Link create notificationLog

--- admin/controllers/app/action.notification.js
+++ admin/controllers/app/action.notification.js
@@ -3,30 +3,59 @@ const debug = Debug('NOWnews-admin-website:controllers:app:action.notification')
 
 module.exports = async (req, res, next) => {
     try{
-
-        let { os } = req.body;
-
-        let options = {
-            url: req.body.newsUrl,
-            title: req.body.newsTitle,
-            summary: req.body.newsSummary,
-            image: req.body.newsImage
+        const CreatedBy = req.session.adminUser._id;
+        const reqBody = req.body;
+        const NewsId = reqBody.NewsId === '' ? null : reqBody.NewsId;
+        let { os } = reqBody;
+        const osArray = (typeof os === 'STRING') ? [os] : os;
+        const options = {
+            url: reqBody.newsUrl,
+            title: reqBody.newsTitle,
+            summary: reqBody.newsSummary,
+            image: reqBody.newsImage
         };
+        let notificationResult = [];
         debug('options = %j', options);
 
-        const osArray = (typeof os === 'STRING') ? [os] : os;
 
+        // faild, successd
         if(osArray.indexOf('IOS') > -1) {
             let { data: result } = await axios.post('/app/notification/ios', options);
+            notificationResult.push({
+                NewsId,
+                CreatedBy,
+                os: 'IOS',
+                successCount: result.successd,
+                expectCount: result.successd + result.faild,
+                ...options
+            });
         }
 
         if(osArray.indexOf('ANDROID') > -1) {
             let { data: result } = await axios.post('/app/notification/android', options);
+            notificationResult.push({
+                NewsId,
+                CreatedBy,
+                os: 'ANDROID',
+                successCount: result.successd,
+                expectCount: result.successd + result.faild,
+                ...options
+            });
         }
 
         if(osArray.indexOf('WEB') > -1) {
             let { data: result } = await axios.post('/app/notification/web', options);
+            notificationResult.push({
+                NewsId,
+                CreatedBy,
+                os: 'WEB',
+                successCount: result.successd,
+                expectCount: result.successd + result.faild,
+                ...options
+            });
         }
+
+        await axios.post('/notificationLog', notificationResult);
 
         return res.redirect('/app/notification');
     }
